e6552c871b269e71984064fb79fe56f43ad9bcb4
Narmi002 <shaiknarmi002@gmail.com>
2025-09-24 11:32:37 +0530

    update videos

--- Components/Home.jsx
+++ Components/Home.jsx
@@ -166,11 +166,11 @@ export default function HeroSection2025() {
 
         </div>
       </section>
+      <section className="w-full px-4 md:px-8 overflow-hidden"><VideoCarousel /></section>
       <section className="w-full px-4 md:px-8 overflow-hidden"><DirectorsSection/></section>
       <section className="w-full px-4 md:px-8 overflow-hidden"><DeansSection/></section>
 
 
-      <section className="w-full px-4 md:px-8 overflow-hidden"><VideoCarousel /></section>
       <section className="w-full px-4 md:px-8 overflow-hidden"><CommitteesSection /></section>
       <section className="w-full px-4 md:px-8 overflow-hidden"><ParticipatingIITs /></section>
     </>
--- Components/VideoCarousel.jsx
+++ Components/VideoCarousel.jsx
@@ -7,48 +7,47 @@ const videos = [
   "/video/madras1.mp4",
   "/video/madras2.mp4",
   "/video/directors.mp4",
-    "/video/directors.mp4",
+  "/video/directors.mp4",
 
 ];
 
 export default function VideoCarousel() {
- const settings = {
-  dots: true,
-  infinite: true,
-  speed: 500,
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  centerMode: true,
-  centerPadding: "0px",
-  responsive: [
-    {
-      breakpoint: 1024, // tablets
-      settings: {
-        slidesToShow: 2,
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    centerMode: true,
+    centerPadding: "0px",
+    responsive: [
+      {
+        breakpoint: 1024, // tablets
+        settings: {
+          slidesToShow: 2,
+        },
       },
-    },
-    {
-      breakpoint: 640, // mobile
-      settings: {
-        slidesToShow: 1,
+      {
+        breakpoint: 640, // mobile
+        settings: {
+          slidesToShow: 1,
+        },
       },
-    },
-  ],
-};
+    ],
+  };
 
   return (
     <div className="cursor-pointer container mx-auto my-10">
       <h3 className="text-center text-2xl font-semibold mb-6">Inter IIT Sports 2025 videos</h3>
       <Slider {...settings}>
         {videos.map((video, index) => (
-<div key={index} className="px-2 w-full">
+          <div key={index} className="px-2 w-full">
             <video
-              src={video}
-              className="rounded-lg shadow-lg w-full"
-              loop
-              autoPlay
-              muted
+              src={index === 0 ? video : undefined}
+              data-src={video}
+              onMouseEnter={(e) => e.currentTarget.src = e.currentTarget.dataset.src}
               controls
+              muted
             />
           </div>
         ))}
